582224c74627c533f9a39166e77ffcc845b34289
Shukhratjon Rayimjonov <shrayimjonov@cdti.uz>
2023-06-19 18:26:09 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello java!");
+        System.out.println("This project is  repetition purpose only");
+    }
+}
